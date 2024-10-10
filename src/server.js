f3ac0610125c8ddee236920ffd5a66b8eccf3251
igordmouraa <igordmoura_@hotmail.com>
2024-10-10 13:30:21 -0300

    refactor(server): change app port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./index');
-const PORT = process.env.PORT;
+const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado e rodando na porta ${PORT} 💾`);
