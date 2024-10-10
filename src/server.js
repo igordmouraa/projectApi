6f05d6506fb818ebf7f959dd1de0ec2811aadc0f
igordmouraa <igordmoura_@hotmail.com>
2024-10-10 13:38:56 -0300

    refactor(server): change app port if don't found the env port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./index');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado e rodando na porta ${PORT} 💾`);
